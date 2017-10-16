529f1f10a313fe4109c8728492386728d9eb5ff4
Vamsinator <vamsi.ghorakavi@utexas.edu>
2017-10-16 09:41:22 -0500

    Made pointline look more like .h file

--- drivers/src/pointline/pointline/pointline.ino
+++ drivers/src/pointline/pointline/pointline.ino
@@ -1,60 +1,48 @@
 /*
-  Analog Input
- Demonstrates analog input by reading an analog sensor on analog pin 0 and
- turning on and off a light emitting diode(LED)  connected to digital pin 13. 
- The amount of time the LED will be on and off depends on
- the value obtained by analogRead(). 
- 
- The circuit:
- * Potentiometer attached to analog input 0
- * center pin of the potentiometer to the analog pin
- * one side pin (either one) to ground
- * the other side pin to +3.3V
- * LED anode (long leg) attached to digital output 13
- * LED cathode (short leg) attached to ground
- 
- * Note: because most Arduinos have a built-in LED attached 
- to pin 13 on the board, the LED is optional.
- 
- 
- Created by David Cuartielles
- modified 30 Aug 2011
- By Tom Igoe
- 
- This example code is in the public domain.
- 
- http://arduino.cc/en/Tutorial/AnalogInput
+Code to use a single point line sensor
  
  */
 
-int sensorPin = A7;    // select the input pin for the potentiometer
-//int ledPin = 13;      // select the pin for the LED
+int sensorPin = A7;    // select the input pin for the Line sensor
 int sensorValue = 0;  // variable to store the value coming from the sensor
+typedef enum {LINE, NOLINE, UNKNOWN} lineStatus;
 #define BLACK 2700
 #define WHITE 1000
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  //pinMode(ledPin, OUTPUT);  
+  //Start serial Monitor 
+  pinMode(GREEN_LED, OUTPUT);
   Serial.begin(9600);
 }
 
+lineStatus getValue(int sensorValue)
+{
+    if(sensorValue<WHITE)
+  {
+    return NOLINE;
+  }
+  if(sensorValue>BLACK)
+  {
+    return LINE;
+  }
+  return UNKNOWN;
+}
 void loop() {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);    
   // turn the ledPin on
-  //digitalWrite(ledPin, HIGH);  
+  digitalWrite(GREEN_LED, HIGH);  
   // stop the program for <sensorValue> milliseconds:
-  if(sensorValue<WHITE)
-  {
-    Serial.println("WHITE");
-  }
-  if(sensorValue>BLACK)
+  lineStatus line = getValue(sensorValue);
+  if(line==LINE)
   {
     Serial.println("BLACK");
   }
-
+  if(line==NOLINE)
+  {
+    Serial.println("WHITE");
+  }
   // turn the ledPin off:        
-  //digitalWrite(ledPin, LOW);   
+  digitalWrite(GREEN_LED, LOW);   
   // stop the program for for <sensorValue> milliseconds:
   delay(1.0);                  
 }
