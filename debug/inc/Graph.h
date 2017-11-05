2c9fd5cd34c9e9ac317177e9de69693e37e1ec62
cole-thompson <colethompson@utexas.edu>
2017-11-05 17:41:36 -0600

    graph header

--- debug/inc/Graph.h
+++ debug/inc/Graph.h
@@ -1,27 +1,18 @@
 
 #include "Node.h"
+#include <HashMap.h>
+//https://playground.arduino.cc/Code/HashMap		 graph[0](Node,Node[8]);
 
 class Graph		//class or struct?
 {
-protected:	
+protected:
 	int V;
-	Node *nodeList;  	//should the position in the list of index i reflect the relative position to i? 
-						//have 8[?] positions for each list, and NULL nodes for directions that don't
-						//use an array instead
+	HashMap<Node, Node[8]> graph;
 
 public:
-	Graph()
-	{
-		bruteForceInit();
-	}
-
-	//need functions to move through map and get/set node details?
+	Graph();
+	Node* getNode(Node::Name n);
 
 private:
-	void bruteForceInit()
-	{
-		//creates the map with all of the connections and specific nodes
-		//how to do this more easily?
-	}
-
+	void bruteForceInit();
 }
