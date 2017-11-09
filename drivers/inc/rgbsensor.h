20407af59a17c1a57a17b8bc29b8b4fc246a63cd
chadharthan <32985726+chadharthan@users.noreply.github.com>
2017-11-08 21:08:59 -0600

    Update rgbsensor.h

--- drivers/inc/rgbsensor.h
+++ drivers/inc/rgbsensor.h
@@ -1,33 +1,15 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
-// Pick analog outputs, for the UNO these three work well
-// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
-#define redpin 3
-#define greenpin 5
-#define bluepin 6
-// for a common anode LED, connect the common pin to +5V
-// for common cathode, connect the common to ground
-
-// set to false if using a common cathode LED
-#define commonAnode true
-
-// our RGB -> eye-recognized gamma color
-
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
-
 class rgbsensor
 {
   public: 
-  rgbsensor(int redPin, int greenPin, int bluePin);
+  rgbsensor();
   String getColor();
   
   private:
-  int rPin;
-  int gPin;
-  int bPin;
   String getClosestColor(int red,int green, int blue);
+  Adafruit_TCS34725 tcs;
   
 };
 
