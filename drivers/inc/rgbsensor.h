02d405dbca5818dd6927c30ca057db36ffd1a4ac
chadharthan <32985726+chadharthan@users.noreply.github.com>
2017-11-08 20:40:55 -0600

    Update rgbsensor.h

--- drivers/inc/rgbsensor.h
+++ drivers/inc/rgbsensor.h
@@ -13,7 +13,6 @@
 #define commonAnode true
 
 // our RGB -> eye-recognized gamma color
-byte gammatable[256];
 
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
