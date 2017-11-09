a4afe40fcdf5dd951a5bc2afeaa7747841875120
chadharthan <32985726+chadharthan@users.noreply.github.com>
2017-11-08 21:08:14 -0600

    Update rgbsensor.cpp

--- drivers/src/rgbsensor.cpp
+++ drivers/src/rgbsensor.cpp
@@ -2,26 +2,11 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
-// Pick analog outputs, for the UNO these three work well
-// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
-#define redpin 3
-#define greenpin 5
-#define bluepin 6
-// for a common anode LED, connect the common pin to +5V
-// for common cathode, connect the common to ground
-
-// set to false if using a common cathode LED
-#define commonAnode true
-
-// our RGB -> eye-recognized gamma color
-byte gammatable[256];
-
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
-
-void setup() {
+rgbsensor::rgbsensor() {
   Serial.begin(9600);
   Serial.println("Color View Test!");
+  
+  tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
   if (tcs.begin()) {
     Serial.println("Found sensor");
@@ -29,25 +14,13 @@ void setup() {
     Serial.println("No TCS34725 found ... check your connections");
     while (1); // halt!
   }
-  
-  // use these three pins to drive an LED
-  pinMode(redpin, OUTPUT);
-  pinMode(greenpin, OUTPUT);
-  pinMode(bluepin, OUTPUT);
  
 }
 
-
-void loop() {
+String rgbsensor::getColor(){
   uint16_t clear, red, green, blue;
 
-  tcs.setInterrupt(false);      // turn on LED
-
   delay(60);  // takes 50ms to read 
-  
-  tcs.getRawData(&red, &green, &blue, &clear);
-
-  tcs.setInterrupt(true);  // turn on LED
   
   /*Serial.print("C:\t"); Serial.print(clear);
   Serial.print("\tR:\t"); Serial.print(red);
@@ -55,10 +28,14 @@ void loop() {
   Serial.print("\tB:\t"); Serial.println(blue);
   Serial.println(getColor(red,green,blue));  
   delay(500);*/
+  
+  tcs.getRawData(&red, &green, &blue, &clear);
 
+  tcs.setInterrupt(true);  // turn on LED
+  return getClosestColor(red,green,blue);
 }
 
-String getColor(int red,int green, int blue)
+String rgbsensor::getClosestColor(int red,int green, int blue)
 {
   int threshold = 200;
   int whiteThreshold = 1500;
