a3de1b80302b9f4ad7c806465291aada8ddf7fd1
Vamsinator <vamsi.ghorakavi@utexas.edu>
2017-11-19 22:29:16 -0600

    Started Log File code
    -Use EEPROM to write to log files
    -Added a header sequence to look for in EEPROM
    -Edited .h file to have more helper functions

--- debug/inc/Log.h
+++ debug/inc/Log.h
@@ -1,9 +1,15 @@
 #include "Graph.h"
+#define MAX_MEMORY 4096
 
 class Log {
 public:
   Log();
   void logNode(Node n);
   void clearLog();
+protected:
+  String startSequence = "START HERE";
+private:
+  dataAddr = 0;
+  static void writeString(String str);  
 
-}
+};
