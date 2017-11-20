31a59d049bd389c385f30961a06e90a06435fac4
ZachPope531 <zachary.pope531@gmail.com>
2017-11-19 18:19:18 -0600

    Made the encoder class, changed up the motorController class, and need to make the encoderReceiver class and connect everything

--- drivers/src/motorController.cpp
+++ drivers/src/motorController.cpp
@@ -1,19 +1,16 @@
 #include "../inc/motorController.h"
 
-oneRotation = 3.1 * 2 * 3.14159265358979323846;
-
-
 motorController::motorController(int _boardAddress, int _shieldAddress){
-	
+
 	Wire.onReceive(receiveEvent);
 	Wire.begin(0x10);
 	//Attach the board to the shield
 	Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
-	
+
 	//Attach the motors
 	Adafruit_DCMotor *rightMotor = AFMS.getMotor(1);
 	Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
-	
+
 	//Default frequency: 1.6 KHz
 	AFMS.begin();
 	//AFMS.begin(1000); //1000Hz for example
@@ -36,15 +33,32 @@ void motorController::moveForward(double distance){
 }
 
 void motorController::moveBackward(double distance){
-	//Have to get the encoders working
+	rightMotor->run(BACKWARD);
+	leftMotor->run(BACKWARD);
+	rightMotor->setSpeed(255);
+	rightMotor->setSpeed(255);
+	//Have to make a encoder data receiver I think
 }
 
 void motorController::turn(double angleDegrees){
-	//Have to get the encoders working
+	if(angleDegrees > 0){
+		rightMotor->run(BACKWARD);
+		leftMotor->run(FORWARD);
+		rightMotor->setSpeed(127);
+		leftMotor->setSpeed(127);
+		//Make something for the gyro to tell how much of an arc we have to cover
+	} else if(angleDegrees < 0){
+		rightMotor->run(FORWARD);
+		leftMotor->run(BACKWARD);
+		rightMotor->setSpeed(127);
+		leftMotor->setSpeed(127);
+	} else if(angleDegrees == 0){
+		moveForward(distance);
+	}
 }
 
 //Not part of the motorController class
 //What happens when the Mega asks for it to move?
 void receiveEvent(int numBytes){
-	
+
 }
