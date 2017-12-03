71baa4bd0fa1b23db53820b6ffab3c2a90130daf
Kiran Raja <kiran.raja22@gmail.com>
2017-12-03 17:58:29 -0600

    testGraph stuff

--- debug/tests/testGraph.cpp
+++ debug/tests/testGraph.cpp
@@ -1,3 +1,8 @@
+using namespace std;
+
 int main(){
-	
+	Graph g = new Graph();
+	g.setCurrentNode(g.getNode(Node::Name::Y));
+	g.setCurrentDirection(1);
+	cout << "Current Node is: " <<g.getCurrentNode() << "\nCurrent Direction is: " << g.getCurrentDirection();
 }
