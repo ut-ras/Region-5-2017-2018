f1ffa1941243ce5cf0d2221d6942db34b411c241
Kiran Raja <kiran.raja22@gmail.com>
2017-12-03 18:07:57 -0600

    testGraph

--- debug/tests/testGraph.cpp
+++ debug/tests/testGraph.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main(){
 	Graph g = new Graph();
 	g.setCurrentNode(g.getNode(Node::Name::Y));
-	g.setCurrentDirection(1);
-	cout << "Current Node is: " <<g.getCurrentNode() << "\nCurrent Direction is: " << g.getCurrentDirection();
+	g.setCurrentDirection(0);
+	cout << "Current Node is: " << g.getCurrentNode() << "\nCurrent Direction is: " << g.getCurrentDirection();
+	if(g.getNeighbor(g.getCurrentNode(), 0).getName() ==  Node::Name::M)
+		cout << "Get neighbor test 1 passed";
+	else
+		cout << "Get neighbor test 1 failed";
 }
