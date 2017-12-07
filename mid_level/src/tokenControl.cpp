0cc87605178eab0aeeea3e664d2223788f47cd59
AmrEl-Azizi <elaziziamr@gmail.com>
2017-12-06 20:50:02 -0600

    Addeed comments and clarification to tokenControl

--- mid_level/src/tokenControl.cpp
+++ mid_level/src/tokenControl.cpp
@@ -1,28 +1,39 @@
 #include "../inc/tokenControl.h"
 
+//Distance from maxheight to ground
 #define maxHeight 10.5
+//Distance to fall into the funnel
 #define funnelHeight 5.0
+//Sets the direction of the magnet
 #define up true
 #define down false
+//Delay to allow tokens to latch to magnet
 #define pickupTime 1000;
+//Delay pre and post drop off for stabalization
 #define dropTime 250;
 //Center to first funnel is 50 degrees
 // 43.33333 between funnels
+//Degrees from center to RGB Sesnor
 #define toRGBSensor 20
+//Degrees between each funnel
 #define tokenToToken 43.33333333333333333333333333333333333333333333333333333333333333333333333333333333
+//Degrees to first funnel from the RGB Sensor
 #define toFirstToken 30
+//Pin for the stepper motor
 #define stepperPin 10
 
 //Public
 
 tokenControl::tokenControl() {
-    diskController = *(new stepper(stepperPin));
-    pulleyController = *(new servo());
-    tokenReader = *(new rgbsensor());
-    magnetController = *(new magnet());
+    //Initialize starter variables
+    diskController(stepperPin);
+    pulleyController();
+    tokenReader();
+    magnetController();
 }
 
 void tokenControl::pickUpToken() {
+    //Drops magnet full distance, turns it on and waits for tokens, then returns it to base height
     lowerMagnet(maxHeight);
     magnetController.magnetOn();
     delay(pickupTime);
@@ -30,23 +41,27 @@ void tokenControl::pickUpToken() {
 }
 
 Node::Color tokenControl::storeToken() {
+    //Reads token colour and if there is no token returns the electromagnet
     Node::Color colour = tokenReader.getColor();
     if(colour == Node::electromagnet)
         return colour;
+    //Rotates the disk to the correct funnel, drops the tokens, then resets magnet
     rotateDisktoColor(colour);
     depositInFunnel();
     resetDisk(colour);
-    delete(colour);
 }
 
 void tokenControl::depositTokens(Node::Color c) {
+    //Moves to the correct funnel and picks up all the tokens
     rotateDiskToColor(c);
     pickupFromFunnel();
+    //Moves token to center then deposits the tokens
     resetDisk(c);
     lowerMagnet(maxHeight);
     delay(dropTime);
     magnetController.magnetOff();
     delay(dropTime);
+    //Reset the magnet
     raiseMagnet(maxHeight);
 }
 
@@ -61,6 +76,7 @@ void tokenControl::raiseMagnet(double distance) {
 }
 
 void tokenControl::rotateDiskToColor(Node::Color c) {
+    //Finds most efficient direction for rotation
     if(c > 3){
         diskController.rotateDisk(toFirstToken + toRGBSensor, stepper::COUNTERCLOCKWISE);
         diskController.rotateDisk((7-c)*tokenToToken, stepper::COUNTERCLOCKWISE);
@@ -71,6 +87,7 @@ void tokenControl::rotateDiskToColor(Node::Color c) {
 }
 
 void tokenControl::resetDisk(Node::Color c) {
+    //Finds most efficient direction for rotation
     if(c > 3){
         diskController.rotateDisk(toFirstToken + toRGBSensor, stepper::CLOCKWISE);
         diskController.rotateDisk((7-c)*tokenToToken, stepper::CLOCKWISE);
