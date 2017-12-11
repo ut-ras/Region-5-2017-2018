43e774c318b23706adf2918c4ea8d72faaa1f209
Ojas Patel <patelojasv@gmail.com>
2017-12-10 18:06:07 -0600

    Added high level code and merged with code

--- Region-5-2017-2018.ino
+++ Region-5-2017-2018.ino
@@ -2,38 +2,79 @@
 #include "rgbsensor.h"
 #include "tokenControl.h"
 #include "intersectionSensors.h"
+#include <Time.h>
 
 void setup() {
   // put your setup code here, to run once:
-
+  setupMotorsTest();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  loopMotorsTest();
 
 }
 
 void round1(){
 	int[] inventory = new int[6];
+  time_t t = now();
 	if(whichSide() == Node::Color::yellow){
+  //Collection
 		turn(PI / 4);
+   //collect across diagonal going from yellow to red
 		for(int i = 0; i < 4; i++){
 			forwardToIntersection();
 			pickUpToken();
-			updateInventory(storeToken());
+			updateInventory(storeToken(), inventory);
 		}
 		moveForward(distance across grey square);
 		for(int i = 0; i < 4; i++){
 			forwardToIntersection();
 			pickUpToken();
-			updateInventory(storeToken());
+			updateInventory(storeToken(), inventory);
 		}
-		depositIfEnough("red");
+		depositIfEnough(Node::Color::red, 3, inventory[0]);
+   //go to green
+    turn(3 * PI / 4);
+    forwardToIntersection();
+    depositIfEnough(Node::Color::green, 3, inventory[1]);
+    //go across to magenta
+    turn(PI / 2);
+    for(int i = 0; i < 4; i++){
+      forwardToIntersection();
+      pickUpToken();
+      updateInventory(storeToken(), inventory);
+    }
+    moveForward(distance across grey square);
+    for(int i = 0; i < 4; i++){
+      forwardToIntersection();
+      pickUpToken();
+      updateInventory(storeToken(), inventory);
+    }
+    depositIfEnough(Node::Color::magenta, 3, inventory[4]);
+    turn(PI/2);
+    forwardToIntersection();
+    depositIfEnough(Node::Color::cyan, 3, inventory[5]);
+    //go across the diagonal from cyan to blue
+    turn(3 * PI / 4);
+    for(int i = 0; i < 4; i++){
+      forwardToIntersection();
+      pickUpToken();
+      updateInventory(storeToken(), inventory);
+    }
+    moveForward(distance across grey square);
+    for(int i = 0; i < 4; i++){
+      forwardToIntersection();
+      pickUpToken();
+      updateInventory(storeToken(), inventory);
+    }
+    depositIfEnough(Node::Color::blue, 3, inventory[2]);
+    //---------------------------------------------------
+    
 
 
-
-	} else{
+	} else{ // first color is red
+  
 
 	}
 }
@@ -51,10 +92,13 @@ void forwardToIntersection(){
 		moveForward();
 }
 
+void updateInventory(Node::Color c, int[] inventory){
+  inventory[c]++;
+}
+
 void depositIfEnough(Node::Color c, int min, int current){
 	if(current >= min)
 		tokenControl::depositTokens();
-
 }
 
 Node::Color whichSide(){
