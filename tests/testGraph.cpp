d74dffd1d6bd21eda7f3841c996a763155873ddc
cole-thompson <colethompson@utexas.edu>
2018-01-22 20:37:55 -0600

    graph test

--- tests/testGraph.cpp
+++ tests/testGraph.cpp
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "../control/inc/Graph.h"
-#include <"/Users/cathyhulu/Downloads/Arduino.app/Contents/Java/hardware/arduino/avr/libraries/HashMap/HashMap.h"
 
-using namespace std;
+Graph g;
 
-int main(){
-	Graph g = new Graph();
+void setupGraphTest() {
+	g = new Graph();
 	g.setCurrentNode(g.getNode(Node::Name::Y));
 	g.setCurrentDirection(0);
+
+
+void loopGraphTest() {
 	cout << "Current Node is: " << g.getCurrentNode() << "\nCurrent Direction is: " << g.getCurrentDirection();
 	if(g.getNeighbor(g.getCurrentNode(), 0).getName() ==  Node::Name::M)
 		cout << "Get neighbor test 1 passed";
 	else
 		cout << "Get neighbor test 1 failed";
+}
 }
