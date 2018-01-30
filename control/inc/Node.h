d3ac2e47449a5973876ddb0bf99ec2bf1688a29b
Catherine Lu <catherinelu@utexas.edu>
2018-01-29 20:33:37 -0600

    addresses enum compilation errors

--- control/inc/Node.h
+++ control/inc/Node.h
@@ -2,47 +2,50 @@
 
 
 //base class
+
+enum Color {red, green, blue, yellow, magenta, cyan, grey, unknown};
+enum Name { R, R1, R2, R3, R4, R5, 
+			G, G1, G2, G3, G4, G5, 
+			B, B1, B2, B3, B4, B5, 
+			Y, Y1, Y2, Y3, Y4, Y5, 
+			M, M1, M2, M3, M4, M5, 
+			C, C1, C2, C3, C4, C5, 
+			X};	//'X' is grey cuz it's the 'crossroads'
+
 class Node
 {
+	public:
+
+		void Node();
+		void Node(int n, int mc, int d); //n = Name, mc = Color
+		int getName();
+		void setName(int n); //n = Name
+		bool getVisited();
+		void setVisited(bool visit);
+		int getMapColor();
+
+		String toString();
+
 	protected:
-	enum Color {red, green, blue, yellow, magenta, cyan, grey, unknown};
-	enum Name { R, R1, R2, R3, R4, R5, 
-				G, G1, G2, G3, G4, G5, 
-				B, B1, B2, B3, B4, B5, 
-				Y, Y1, Y2, Y3, Y4, Y5, 
-				M, M1, M2, M3, M4, M5, 
-				C, C1, C2, C3, C4, C5, 
-				X};	//'X' is grey cuz it's the 'crossroads'
 
-	Name name;
-	Color mapColor;
-	int depth;
+		Name name;
+		Color mapColor;
+		int depth;
 
-	Color tokenColor;
-	bool visited;
-	Node* adjList;
-	String[] nameTable = {"Red Box", "Red 1", "Red 2", "Red 3", "Red 4", "Red 5",
-	                      "Green Box", "Green 1", "Green 2", "Green 3", "Green 4", "Green 5",
-						  "Blue Box", "Blue 1", "Blue 2", "Blue 3", "Blue 4", "Blue 5",
-						  "Yellow Box", "Yellow 1", "Yellow 2", "Yellow 3", "Yellow 4", "Yellow 5",
-						  "Magenta Box", "Magenta 1", "Magenta 2", "Magenta 3", "Magenta 4", "Magenta 5",
-						  "Cyan Box", "Cyan 1", "Cyan 2", "Cyan 3", "Cyan 4", "Cyan 5",
-						  "Grey Box"};
+		Color tokenColor;
+		bool visited;
+		Node* adjList;
+		String[] nameTable = {"Red Box", "Red 1", "Red 2", "Red 3", "Red 4", "Red 5",
+		                      "Green Box", "Green 1", "Green 2", "Green 3", "Green 4", "Green 5",
+							  "Blue Box", "Blue 1", "Blue 2", "Blue 3", "Blue 4", "Blue 5",
+							  "Yellow Box", "Yellow 1", "Yellow 2", "Yellow 3", "Yellow 4", "Yellow 5",
+							  "Magenta Box", "Magenta 1", "Magenta 2", "Magenta 3", "Magenta 4", "Magenta 5",
+							  "Cyan Box", "Cyan 1", "Cyan 2", "Cyan 3", "Cyan 4", "Cyan 5",
+							  "Grey Box"};
 
-    String[] colorTable = {"red", "green", "blue", "yellow", "magenta", "cyan", "grey", "unknown"}
+	    String[] colorTable = {"red", "green", "blue", "yellow", "magenta", "cyan", "grey", "unknown"}
 
-public:
-	
 
-	void Node();
-	void Node(int n, int mc, int d); //n = Name, mc = Color
-	int getName();
-	void setName(int n); //n = Name
-	bool getVisited();
-	void setVisited(bool visit);
-	int getMapColor();
-
-	String toString();
 
 	/* Use the lookup  tables for these values
 	char colorToChar(Color c);
