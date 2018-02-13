4ac1f5d5cabbadd93abbb0978cfce2dd57ae1fa3
Kiran Raja <kiran.raja22@gmail.com>
2018-02-12 20:06:34 -0600

    adjusted servo test

--- src/tests/testServo.cpp
+++ src/tests/testServo.cpp
@@ -8,8 +8,8 @@ void setupServoTest(){
 }
 
 void loopServoTest(){
-	servo.movePulley(1, true);
-	Serial.println("Moved down 1 unit...");
-	servo.movePulley(1, false);
-	Serial.println("Moved back up 1 unit...");
+	servo.movePulley(180);
+	Serial.println("Moved to position 180");
+	servo.movePulley(160);
+	Serial.println("Moved to position 160");
 }
