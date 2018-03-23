49b27f5900ad0635429ef2ef41eb663441671fef
cole-thompson <colethompson@utexas.edu>
2018-03-23 17:42:31 -0500

    motor test

--- R5MegaDrive/R5MegaDrive.ino
+++ R5MegaDrive/R5MegaDrive.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include "MotorControl.h"
 
+enum Commands{m1f, m2f, m3f, m1b, m2b, m3b, t1f, t1b, s};
+
 MotorControl m;
 
 void setup() {
@@ -10,12 +12,19 @@ void setup() {
 
   Wire.begin(8);
   Wire.onReceive(receiveEvent);
+
+  testMotorSetup();
 }
 
 void loop() {
   delay(5);
   m.update();
 }
+
+void testMotorSetup() {
+  m.move(true);
+}
+
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
@@ -25,32 +34,30 @@ void receiveEvent(int howMany) {
 
     switch(c){
       case m:
-        
+
         break;
       case m:
-        
+
         break;
       case m:
-        
+
         break;
       case m:
-        
+
         break;
       case m:
-        
+
         break;
       case m:
-        
+
         break;
       case m:
-        
+
         break;
 
-        
-        
-      
+
+
+
     }
   }
 }
-
-
