02c3d51cfd9d9a73ad37b8e5dfb2dfd1e6521cf2
Vamsinator <vamsi.ghorakavi@utexas.edu>
2018-04-06 02:39:56 -0500

    Merge branch 'master' of https://github.com/ut-ras/Region-5-2017-2018

--- R5MegaDrive/MotorControl.cpp
+++ R5MegaDrive/MotorControl.cpp
@@ -139,8 +139,8 @@ void MotorControl::stopMotors(int lastCmd) {
 
   int loopCount = 0;
   while ((r_pos_last != r_pos_current) || (l_pos_last != l_pos_current)) {
-    r_pos_last = r_current;
-    l_pos_last = l_current;
+    r_pos_last = r_pos_current;
+    l_pos_last = l_pos_current;
     r_pos_current = r_Encoder->getPos();
     l_pos_current = l_Encoder->getPos();
     delay(waitTimeMs);
