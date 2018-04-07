f40f0e50de58d078a631e010dd89fc949efed6c7
Cole Thompson <colethompson@utexas.edu>
2018-04-07 04:24:49 -0500

    lol

--- R5MegaMain/src/control/driveControl.cpp
+++ R5MegaMain/src/control/driveControl.cpp
@@ -39,6 +39,7 @@ void driveControl::move(bool fwd) {
 void driveControl::turnManeuver(bool left, int steps) {
   Serial.println("In turnManeuver " + String(left?"left":"right"));
   sendCommand(left?(LEFT45 + steps - 1):(RIGHT45 + steps - 1));
+  delay(4000);   //wait to finish turning!!!
 }
 
 void driveControl::stop() {
